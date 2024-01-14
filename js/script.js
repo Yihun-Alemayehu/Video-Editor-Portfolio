b5b429438c9f6366c8e3d4894bf186c4cceab62d
Yihun Alemayehu <yankure01@gmail.com>
2024-01-15 00:23:29 +0300

    fdb-14

--- js/script.js
+++ js/script.js
@@ -8,10 +8,27 @@ let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
-
+ 
+    
 
     // sticky header
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop -100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
 
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(Links => {
+                Links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            })
+        }
+    })
+
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
 
     // remove toggle icon and navbar when click navbar links (scroll)
 
