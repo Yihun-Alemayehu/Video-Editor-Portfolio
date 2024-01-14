b2c97d9d769470263f6d585880f8c123c8ca80e4
Yihun Alemayehu <yankure01@gmail.com>
2024-01-14 10:29:18 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+
+// scroll sections
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+
+
+    // sticky header
+
+
+    // remove toggle icon and navbar when click navbar links (scroll)
+
+
+    // animation footer on scroll
+
+}
